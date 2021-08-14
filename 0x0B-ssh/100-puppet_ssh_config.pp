b2da3824b0b85a8a2fcec06410c4dca24f222b11
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-08-14 18:48:44 +0000

    Puppet to make changes to our configuration file.

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,12 @@
 # Using Puppet, set up your client SSH configuration file so that you can connect to a server without 
-file { '~/.ssh/config':
-  ensure  => present,
-  replace => 'yes',
-  path    => '~/.ssh/config'
-  content => 'Host *
-     HostName 34.139.184.21
-     User 29026aabb769
-     IdentityFile ~/.ssh/holberton',
+file_line { 'TurnOffPasswordAuthy':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'DeclareIdentityFile':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/holberton',
 }
