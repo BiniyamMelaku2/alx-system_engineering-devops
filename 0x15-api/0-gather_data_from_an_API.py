884c212ba145de1a963f214f66e5636b34d854a1
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-09-15 16:51:05 +0000

    returns employee TODO list

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ given employee ID, returns information about his/her TODO list progress """
-from sys import argv
 from requests import get
+from sys import argv
 
 
 if __name__ == "__main__":
