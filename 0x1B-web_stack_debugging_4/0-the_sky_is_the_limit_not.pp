98379e7b9957d6bae24da717c9d3e566e831539b
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-10-07 16:16:17 +0000

    using ApacheBench simulate HTTP requests

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # using ApacheBench simulate HTTP requests to a web server to get failed requests and fix our stack
 exec { 'webstack-debug':
-  environment => ['DIR=/etc/default/nginx',
-                  'OLD=ULIMIT="-n 20"',
-                  'NEW=ULIMIT="-n 2000"'],
-  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo service nginx restart',
-  path        => ['/usr/bin', '/bin'],
-  returns     => [0, 1]
+  command => '/bin/sed -i "s/15/1000/" /etc/default/nginx'
+}
+
+exec { 'nginx':
+  require => Exec['webstack-debug'],
+  command => '/usr/sbin/service nginx restart'
 }
